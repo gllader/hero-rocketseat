5a6e413dae024652788d6615fd79dd19a7492c1d
Guilherme <guilherme.lopes123@hotmail.com>
2020-03-25 15:48:48 -0300

    fix controller

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -13,11 +13,11 @@ module.exports = {
             .limit(5)
             .offset((page-1) * 5)
             .select(['incidents.*', 
-                'ongs.name', 
-                'ongs.email',
-                'ongs.whatsapp',
-                'ongs.city',
-                'ongs.uf']);
+            'ongs.name', 
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf']);
 
         res.header('X-Total-Count', count['count(*)'])
 
